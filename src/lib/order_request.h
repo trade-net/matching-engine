f3e3fe6a944dbe212e88af55d22a31f4ab748a50
Joash <joashcwk@gmail.com>
2024-09-18 23:09:29 +0100

    initial implementation of order_request

--- src/lib/order_request.h
+++ src/lib/order_request.h
@@ -2,9 +2,25 @@
 #define INCLUDED_ORDER_REQUEST
 
 struct OrderRequest{
-	OrderRequest(){}
-	std::shared_ptr<Order> toOrder();
-	std::string security = "";
+	OrderRequest(int id, bool isBuy, int units, int limit, int timestamp, const std::string& security)
+	: id(id)
+	, isBuy(isBuy)
+	, units(units)
+	, limit(limit)
+	, timestamp(timestamp)
+	, security(security)
+	{}
+	std::shared_ptr<Order> toOrder()
+	{
+		return std::make_unique<Order>(id, isBuy, units, limit, timestamp);
+	}
+
+	int id;
+	bool isBuy;
+	int units;
+	int limit;
+	int timestamp;
+	std::string security;
 };
 
 #endif
--- src/svc/main.cpp
+++ src/svc/main.cpp
@@ -1,10 +1,28 @@
 #include <order_book.h>
+#include <order_request.h>
 #include <order.h>
 #include <threadpool.h>
 #include <memory>
 #include <iostream>
 
 int main(){
+	BookManager manager(4);
+
+	std::vector<OrderRequest> requests;
+	requests.emplace_back(1, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(2, false, 12, 100, 1, "AAPL");
+	requests.emplace_back(3, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(4, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(5, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(6, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(7, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(8, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(9, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(10, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(11, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(12, true, 10, 100, 1, "AAPL");
+	requests.emplace_back(13, true, 10, 100, 1, "AAPL");
+
 	ThreadPool pool(4);
 	for(size_t i = 0; i < 100; ++i){
 		pool.enqueue(
